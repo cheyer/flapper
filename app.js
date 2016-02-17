c3d6e77bfdeaf7839ddae28c6e10e526affd8578
Chris Heyer <cheyer@de.ibm.com>
2016-02-17 15:05:44 +0100

    added angular service: factory

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', [
-    '$scope',
-    function ($scope) {
-        $scope.pagetitle = "Welcome to Flapper News!";
-        $scope.posts = [
+app.factory('posts', [function () {
+    var o = {
+        posts: [
             { title: 'Google', link: 'http://google.com', upvotes: 5 },
             { title: 'Twitter', link: 'http://twitter.com', upvotes: 3 },
             { title: 'Facebook', link: 'http://facebook.com', upvotes: 16 },
             { title: 'Amazon', link: 'http://amazon.com', upvotes: 9 },
             { title: 'Youtube', link: 'http://youtube.com', upvotes: 4 }
-        ];
+        ]
+    };
+    return o;
+}]);
+
+app.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function ($scope, posts) {
+        $scope.pagetitle = "Welcome to Flapper News!";
+        $scope.posts = posts.posts;
         $scope.addPost = function () {
             if (!$scope.title || $scope.title === "") { return; }
             $scope.posts.push({ title: $scope.title, link: $scope.link, upvotes: 0 });
